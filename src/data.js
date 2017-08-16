98d0c5911bfd55d9b2cf53d46d0839db251d0d56
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-08-16 16:02:40 +0800

    basic data structure

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const Nodes = [
+	{
+		id : 'table1',
+		fields : [
+			{
+				name : 'col1'
+			},
+			{
+				name : 'col2'
+			}
+		]
+	},
+	{
+		id : 'table2',
+		fields : [
+			{
+				name : 'col1'
+			},
+			{
+				name : 'col2'
+			}
+		]
+	}
+]
+
+const Links = [
+	{
+		n1 : {
+			table : 'table1',
+			field : 'col1'
+		},
+		n2 : {
+			table : 'table2',
+			field : 'col2'
+		}
+	}
+]
+
+export {
+	Nodes,
+	Links
+}
