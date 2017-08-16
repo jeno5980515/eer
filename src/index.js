b7b4c7a859422753b8475842c83a4cf79a2d3d02
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-08-16 16:33:19 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import reducers from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
